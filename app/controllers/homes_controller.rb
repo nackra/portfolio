964f58c712e61ae9a30a4dd7e91fe21d0e163088
EC2 Default User <ec2-user@ip-172-31-7-102.ap-northeast-1.compute.internal>
2023-07-20 03:06:36 +0000

    user

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,5 @@
 class HomesController < ApplicationController
+  def top
+    @posts = Post.left_joins(:favorites).group(:id).order('count(post_id) desc').limit(5)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'search' => 'searches#search'
-  root to: 'homes#top'
+  root 'homes#top'
 
   resources :posts do
     resource :favorites, only: [:create, :destroy]
