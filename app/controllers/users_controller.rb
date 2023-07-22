b4dd1229cec67481c454c6e35dabb11f1ef300ff
EC2 Default User <ec2-user@ip-172-31-7-102.ap-northeast-1.compute.internal>
2023-07-22 14:11:23 +0000

    favorite完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,24 @@ class UsersController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def confirm
+    @user = current_user
+  end
+
+  def destroy
+    @user = current_user
+    @user.destroy
+    flash[:notice] = 'ユーザーを削除しました。'
+    redirect_to :root
+  end
+
+  def favorites
+    @user = User.find(params[:user_id])
+    favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
+    @favorite_posts = Post.find(favorites)
+    @posts = @user.posts.page(params[:page]).reverse_order
+  end
+
   private
 
   def user_params
